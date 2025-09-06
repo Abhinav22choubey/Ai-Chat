fc4e2ae7b58c82069a262f484b379e6733e231ad
Abhinav <your.email@example.com>
2025-09-07 01:40:30 +0530

    Arranging

--- Frontend/main.jsx
+++ Frontend/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import Chatapp from './src/chatapp';
+import Chatapp from './chatapp';
 const Main = () => {
     return (
         <div>
